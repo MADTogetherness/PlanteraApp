966bc29decb1e1cf5b5db8e0ee0b079c0bd2913f
Mr_x_master <kamal20012011@hotmail.com>
2021-12-07 22:59:31 +0400

    Initial Commit - All Basic Settings Done

---
+++ app/src/main/java/com/example/planteraapp/Home.java
@@ -0,0 +1,109 @@
+package com.example.planteraapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuItemImpl;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.Objects;
+
+public class Home extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
+    FloatingActionButton add_plants_fab;
+    NavController navController;
+    NavOptions.Builder options;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        init();
+    }
+
+    @SuppressLint("RestrictedApi")
+    public void init(){
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        add_plants_fab = findViewById(R.id.add_plants_fab);
+        navController = Navigation.findNavController(this, R.id.nav_controller);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        options = new NavOptions.Builder();
+        options.setLaunchSingleTop(true)
+                .setEnterAnim(R.anim.fragment_enter_anim)
+                .setExitAnim(R.anim.fragment_exit_anim)
+                .setPopEnterAnim(R.anim.fragment_popenter_anim)
+                .setPopExitAnim(R.anim.fragment_popexit_anim);
+        bottomNavigationView.getMenu().getItem(0).setChecked(true);
+        add_plants_fab.setOnClickListener(view -> {
+            if(isValidDestination(R.id.newPlant_fragment)){
+                MenuItem menuItem = bottomNavigationView.getMenu().findItem(bottomNavigationView.getSelectedItemId());
+                ((MenuItemImpl) menuItem).setExclusiveCheckable(false);
+                menuItem.setChecked(false);
+                ((MenuItemImpl) menuItem).setExclusiveCheckable(true);
+                Navigation.findNavController(this, R.id.nav_controller).navigate(R.id.newPlant_fragment, null, options.build());
+            }
+        });
+        bottomNavigationView.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(!isValidDestination(getCorrespondingFragmentsId(item.getItemId()))){
+            return false;
+        }
+        switch (item.getItemId()){
+            case R.id.calendar:
+                Navigation.findNavController(this, R.id.nav_controller).navigate(R.id.calendar_fragment, null, options.build());
+                break;
+            case R.id.search:
+                Navigation.findNavController(this, R.id.nav_controller).navigate(R.id.search_fragment, null, options.build());
+                break;
+            case R.id.all_plants:
+                Navigation.findNavController(this, R.id.nav_controller).navigate(R.id.allPlants_fragment, null, options.build());
+                break;
+            case R.id.settings:
+                Navigation.findNavController(this, R.id.nav_controller).navigate(R.id.settings_fragment, null, options.build());
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    private boolean isValidDestination(int destination){
+        return destination != Objects.requireNonNull(Navigation.findNavController(this, R.id.nav_controller).getCurrentDestination()).getId();
+    }
+
+    private int getCorrespondingFragmentsId(int id){
+        switch (id){
+            case R.id.calendar:
+                return R.id.calendar_fragment;
+            case R.id.calendar_fragment:
+                return R.id.calendar;
+            case R.id.search:
+                return R.id.search_fragment;
+            case R.id.search_fragment:
+                return  R.id.search;
+            case R.id.all_plants:
+                return R.id.allPlants_fragment;
+            case R.id.allPlants_fragment:
+                return R.id.all_plants;
+            case R.id.settings:
+                return R.id.settings_fragment;
+            case R.id.settings_fragment:
+                return R.id.settings;
+        }
+        return -1;
+    }
+}
---
+++ app/src/main/java/com/example/planteraapp/Utilities/SlideAdapter.java
@@ -0,0 +1,100 @@
+package com.example.planteraapp.Utilities;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.planteraapp.R;
+
+import java.util.Objects;
+
+public class SlideAdapter extends PagerAdapter {
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SlideAdapter(Context context){
+        this.context = context;
+    }
+    public int[] slide_images ={
+            R.drawable.img_intro1_image,
+            R.drawable.img_intro2_image,
+            R.drawable.img_intro3_image
+    };
+
+    public int[] slide_images_backgroundTint ={
+            R.color.image1,
+            R.color.image2,
+            R.color.image3
+    };
+
+    public String[] headings = {
+            "Manage efficiently",
+            "Care more, Forget less",
+            "Record your progress"
+    };
+
+    public String[] descriptions = {
+            "\nAdd your garden here & manage your plants efficiently all at single place just like a pro...",
+            "\nYou need not worry anymore..We manage all your notifications about your plant needs and remind you timely of it...",
+            "\nAdding more to it, You can also record your plant progress...Just like it's your own child progressing forward!! \n\nLet's get started then..."
+    };
+
+    public int[] nextButton_backgroundTint ={
+            R.color.image1_Button,
+            R.color.image2_Button,
+            R.color.image3_Button
+    };
+
+    public int getNextButton_backgroundTint(int pos){
+        return nextButton_backgroundTint[pos];
+    }
+
+    @Override
+    public int getCount() {
+        return headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        Log.d("OBJECT_VIEW", String.valueOf(position));
+        layoutInflater =(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.intro_slide_layout, container,false);
+        TextView head =  view.findViewById(R.id.intro_head),
+                description =  view.findViewById(R.id.intro_description);
+
+        ImageView Slide_image  = view.findViewById(R.id.intro_image);
+        Slide_image.setBackgroundTintList(ContextCompat.getColorStateList(context,slide_images_backgroundTint[position]));
+        Slide_image.setImageResource(slide_images[position]);
+        head.setText(headings[position]);
+        description.setText(descriptions[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
