25fef5807c99e9779897fbfc064fb989e199eb13
Mr_x_master <kamal20012011@hotmail.com>
2021-12-26 13:03:09 +0400

    FINAL TESTING OF DATABASE DONE

--- app/src/main/java/com/example/planteraapp/homefragments/Settings.java
+++ app/src/main/java/com/example/planteraapp/homefragments/Settings.java
@@ -1,36 +1,10 @@
 package com.example.planteraapp.homefragments;
-
-import android.database.sqlite.SQLiteConstraintException;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.planteraapp.AppDatabase;
 import com.example.planteraapp.R;
-import com.example.planteraapp.Utilities.PickAndReleaseImages;
-import com.example.planteraapp.Utilities.AttributeConverters;
-import com.example.planteraapp.entities.Blog;
-import com.example.planteraapp.entities.DAO.PlantDAO;
-import com.example.planteraapp.entities.Images;
-import com.example.planteraapp.entities.Plant;
-import com.example.planteraapp.entities.Relations.BlogWithImages;
-import com.example.planteraapp.entities.Relations.PlantsWithBlogsANDImages;
-import com.google.android.material.imageview.ShapeableImageView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -39,24 +13,6 @@ import java.util.List;
  */
 public class Settings extends Fragment {
 
-    private TextView extraTextTV;
-    private EditText blogDescET, plantIDET, nameToLoadET;
-    private Button loadData, saveData, pickImages;
-    private List<Images> blogImagesForUpload;
-    private List<String> imageSource, imageName;
-    private ShapeableImageView blogimg;
-    private final long blogID = -1;
-
-
-    private ArrayList<Uri> imageUris;
-
-    private static final int PICK_IMAGES_CODE = 0;
-
-    int pos = 0;
-
-    private View view;
-    private PickAndReleaseImages pickAndReleaseImages;
-    private PlantDAO DAO;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -102,75 +58,5 @@ public class Settings extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        this.view = view;
-        DAO = AppDatabase.getInstance(requireContext()).plantDAO();
-        init();
-    }
-
-    public void init() {
-        blogDescET = view.findViewById(R.id.blog_description);
-        loadData = view.findViewById(R.id.btn_load_data);
-        saveData = view.findViewById(R.id.btn_save_data);
-        extraTextTV = view.findViewById(R.id.extra_text);
-        nameToLoadET = view.findViewById(R.id.name_to_load);
-        plantIDET = view.findViewById(R.id.plant_id);
-        pickImages = view.findViewById(R.id.btn_pick_img);
-        imageUris = new ArrayList<>();
-        imageName = new ArrayList<>();
-        imageSource = new ArrayList<>();
-        blogimg = view.findViewById(R.id.profile_image);
-        pickAndReleaseImages = new PickAndReleaseImages(requireContext(), DAO, requireActivity().getActivityResultRegistry());
-        getLifecycle().addObserver(pickAndReleaseImages);
-        pickImages.setOnClickListener(view -> {
-            pickAndReleaseImages.pickImages();
-        });
-
-        saveData.setOnClickListener(view -> {
-            extraTextTV.setText("");
-            String desc = blogDescET.getText().toString().trim(),
-                    plantName = plantIDET.getText().toString().trim();
-            if (plantName.equals(""))
-                return;
-            Blog newBlog = new Blog(plantName, desc);
-            try {
-                long s = DAO.insertNewBlog(new Blog(plantName, desc));
-                Log.d("insertB", String.valueOf(s));
-                Toast.makeText(requireContext(), "NEW BLOG Inserted : " + newBlog.toString(), Toast.LENGTH_SHORT).show();
-                if (pickAndReleaseImages.ImagePathsAvailable()) {
-                    List<Long> success = pickAndReleaseImages.addBlogsAndImages(s);
-                    for (long ss : success) Log.d("insertI", String.valueOf(ss));
-
-                }
-            } catch (SQLiteConstraintException e) {
-                e.printStackTrace();
-            }
-        });
-
-        loadData.setOnClickListener(view -> {
-            extraTextTV.setText("");
-            String name = nameToLoadET.getText().toString().trim();
-            if (name.equals("")) return;
-            Plant plant = DAO.getSinglePlantInstance(name);
-            if (plant == null) {
-                Toast.makeText(requireContext(), "INVALID NAME", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            PlantsWithBlogsANDImages plantBlogsWithImages = DAO.getPlantWithBlogsANDImages(plant.plantName);
-            if (plantBlogsWithImages == null) {
-                Toast.makeText(requireContext(), "INVALID NAME", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            List<BlogWithImages> all_blog = plantBlogsWithImages.blogs;
-            for (BlogWithImages b : all_blog)
-                extraTextTV.append("Blogs: " + b.blog.blogID + " : " + plant.plantName + " : " + b.blog.description + "\n");
-            blogimg.setImageBitmap(AttributeConverters.StringToBitMap(all_blog.get(0).images.get(0).imageData));
-        });
-
-
     }
 }
