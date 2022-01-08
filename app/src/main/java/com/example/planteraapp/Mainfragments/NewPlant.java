9698063ef0f7200ad4ffe50bd8ef1e39eeaac8bd
Farhan Sadiq <farhan_sadiq@live.com>
2022-01-08 19:04:30 +0400

    reminder items

--- app/src/main/java/com/example/planteraapp/Mainfragments/NewPlant.java
+++ app/src/main/java/com/example/planteraapp/Mainfragments/NewPlant.java
@@ -52,6 +52,8 @@ public class NewPlant<TextView> extends Fragment {
 
     String name[], time[], interval[], lastComp[];
 
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
