947f7639011045f51377aed6fe7f6f95ecf08572
Mr_x_master <kamal20012011@hotmail.com>
2022-01-09 17:54:05 +0400

    Cleared the before Merge Problem

--- app/src/main/java/com/example/planteraapp/Mainfragments/NewPlant.java
+++ app/src/main/java/com/example/planteraapp/Mainfragments/NewPlant.java
@@ -1,12 +1,17 @@
 package com.example.planteraapp.Mainfragments;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
 import com.example.planteraapp.LauncherActivity;
 import com.example.planteraapp.R;
 import com.example.planteraapp.SubFragments.ColorTheme;
@@ -61,6 +66,13 @@ public class NewPlant extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_new_plant, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        view.findViewById(R.id.click).setOnClickListener(v -> {
+            EditPlantTheme();
+        });
     }
 
     // Plant Theme is stored inside variable plantTheme
