dada0622bab126dc19afe4f6bd4e3e3c9a143c53
Mr_x_master <kamal20012011@hotmail.com>
2022-01-15 01:41:42 +0400

    DONE CALENDAR FRAGMENT

--- app/src/main/java/com/example/planteraapp/Mainfragments/Calendar.java
+++ app/src/main/java/com/example/planteraapp/Mainfragments/Calendar.java
@@ -9,71 +9,35 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.room.Dao;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.planteraapp.AppDatabase;
 import com.example.planteraapp.R;
 import com.example.planteraapp.Utilities.ReminderRecyclerAdapter;
 import com.example.planteraapp.entities.DAO.PlantDAO;
-import com.example.planteraapp.entities.Plant;
-import com.example.planteraapp.entities.PlantLocation;
-import com.example.planteraapp.entities.PlantType;
 import com.example.planteraapp.entities.Relations.ReminderAndPlant;
-import com.example.planteraapp.entities.Reminder;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Settings#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Calendar extends Fragment {
     private PlantDAO DAO;
-
-
     private RecyclerView rvTodayReminder, rvTomorrowReminder;
     private ReminderRecyclerAdapter todayReminderRVA, tomorrowReminderRVA;
     private List<List<ReminderAndPlant>> allReminderList;
 
-    public Calendar() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Settings.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Calendar newInstance(String param1, String param2) {
-        Calendar fragment = new Calendar();
-        return fragment;
-    }
+    public Calendar() {/*Required empty public constructor*/}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_calender, container, false);
     }
 
